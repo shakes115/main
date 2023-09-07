987fd916e870e2b4e02eab91da167ce7216b567e
SRailoun <shaakier.railoun115@gmail.com>
2023-09-07 10:05:20 +0200

    modified wrapper.js

--- JavaScript/WrapperActivity/wrapper.js
+++ JavaScript/WrapperActivity/wrapper.js
@@ -1,15 +1,18 @@
-var ul;
-var liItems;
-var imageWidth;
-var imageNumber;
-
-function init(){
-
-    ul = document.getElementById('image_slider');
-    liItems = ul.children;
-    imageNumber = liItems.length;
-    imageWidth = liItems[0].children[0].offsetWidth;
-    //set ul's width as the total width of all images in image slider.
-    ul.style.width = parseInt(imageWidth * imageNumber) + 'px';
-    slider(ul);
+//generic animate function
+function animate(opts){
+    var start = new Date;
+    var id = setInterval(function(){
+        var timePassed = new Date - start;
+        var progress = timePassed / opts.duration
+        if(progress > 1){
+            progress = 1;
+        }
+        var delta = opts.delta(progress);
+        opts.step(delta);
+        if (progress == 1){
+            clearInterval(id);
+            opts.callback();
+        }
+    }, opts.delay || 17);
 }
+window.onload = init;
